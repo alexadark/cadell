fc1fcf9b72693d60a07d7d46e83ad6ab0a3f3fa2
alexaspalato <alexaspalato@gmail.com>
2020-09-28 08:49:39 +0200

    seo

--- src/components/seo/Seo.js
+++ src/components/seo/Seo.js
@@ -9,6 +9,10 @@ const SEO = ({ title = "cadell", description = "Redfining Art Advisory" }) => {
   return (
     <Helmet title={title}>
       <meta name="description" content={description} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta name="twitter:image" content={image} />
     </Helmet>
