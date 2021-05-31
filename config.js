0099f697422eb65c981edaeacecd79be50db36f5
Alexandra Spalato <alexaspalato@gmail.com>
2021-05-31 10:14:39 +0200

    url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * @type {{wordPressUrl: string}}
  */
 const config = {
-  wordPressUrl: `https://cadell.gatsby-wp.com/`,
+  wordPressUrl: `https://cadellco.wpengine.com/`,
 }
 
 module.exports = config
