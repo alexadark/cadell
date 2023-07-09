d2db8126b9a07135bc59b8b4125920a1d2d66f1b
Rene Sauve <rdasauve@gmail.com>
2023-07-09 16:51:51 -0700

    font

--- src/styles/postStyles.js
+++ src/styles/postStyles.js
@@ -1,16 +1,15 @@
 export const postStyles = {
-    '.entry-media':{
-mb: 30,
-    },
-    '.entry-title':{
-        fontSize: 36,
-        mb: 20
-    },
-    '.entry-info':{
-        fontStyle: 'italic',
-        fontSize: 14,
-        mb: 20,
-
-    }
-
+  ".entry-media": {
+    mb: 30,
+  },
+  ".entry-title": {
+    fontSize: 36,
+    mb: 20,
+  },
+  ".entry-info": {
+    fontStyle: "italic",
+    fontSize: 14,
+    fontFamily: "Montserrat, sans-serif",
+    mb: 20,
+  },
 }
